07dffc3e478bb6d1e923592acd2679d8a793060a
Quoiaaaaa <bobropozhalovat@gmail.com>
2017-01-16 23:20:01 +0300

    Study Drills

--- ex10.py
+++ ex10.py
@@ -1,15 +1,17 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
+s = 'full'
+tabby_cat = "\tI'm tabbed in %s." % s
+persian_cat = "I'm split\non a line %r." % s
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 
 I'll do a lsit:
 \t* Cat food
 \t* Fishes
-\t* Catnip\n\t* Grass
+\t* Catnip
+\t* Grass
 
-"""
+'''
 print tabby_cat
 print persian_cat
 print backslash_cat
